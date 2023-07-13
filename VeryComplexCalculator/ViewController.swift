40cb404f984a063ccd4827e4c958e1fd73297779
emir ari <emirariomatic@gmail.com>
2023-07-13 21:33:49 +0300

    Simple config done

--- VeryComplexCalculator/ViewController.swift
+++ VeryComplexCalculator/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    var finishedTyping: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func symbolPressed(_ sender: UIButton) {
+        finishedTyping = true
+    }
+    
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if let num = sender.titleLabel?.text {
+            if finishedTyping {
+                numberLabel.text = num
+                finishedTyping = false
+            } else {
+                numberLabel.text = numberLabel.text! + num
+            }
+        }
+    }
+    
 }
 
